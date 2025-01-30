540ee22497593fe27761a7bb6c5e025122daac7d
sayed <sayed@Sameer.mynetworksettings.com>
2025-01-29 21:10:19 -0500

    added new name

--- src/Demo1/DemoTest1.java
+++ src/Demo1/DemoTest1.java
@@ -4,7 +4,12 @@ public class DemoTest1 {
 	
 	public static void main(String[]args) {
 		
-		System.out.println("Java");
+		
+
+		        String name = "Sameer"; 
+		        
+		     
+		        System.out.println("Hello, " + name + "!");
 		
 	}
 
